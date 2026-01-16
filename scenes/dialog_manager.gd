1d6ee57fe7d63ec40a9f5ca28487df26f943f607
Quinn-Trusler <quinn@trusler.net>
2026-01-16 06:53:20 -0800

    Several dialogs can be chained with time in between

--- scenes/dialog_manager.gd
+++ scenes/dialog_manager.gd
@@ -1,32 +1,34 @@
 extends CanvasLayer
+#something else in the program may want give text to the dialog manager
 
-
-
-var border_size : int = 1
-var dialog : String = "Testing purposes"
+#Time < 0 will be infinite
+var dialogs : Array[Array] = [["12345678919",1],["yadayad",-1]] #[dialog text, seconds]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_border_rect()
-	set_dialog("This is a test")
+	dialog_next()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
-func set_dialog(new_dialog : String) -> void:
-	dialog = new_dialog
-	display_dialog()
+#Sets dialog text and timer based on dialogs array
+func dialog_next() -> void:
+	if len(dialogs) > 0:
+		$DialogBox.set_dialog(dialogs[0][0])
+		if dialogs[0][1] > 0:
+			$Timer.wait_time = dialogs[0][1]
+			$Timer.start()
+		else:
+			$Timer.stop()
+		dialogs.remove_at(0)
+		$DialogBox.visible = true
+	else:
+		$DialogBox.visible = false		
 
-func display_dialog() -> void:
-	$Control/Dialog.text = dialog
+func dialog_ended() -> void:
+	dialog_next()
 
-func set_colour(border : Color,inner_rect : Color,text : Color) -> void:
-	pass
-
-func set_border_rect() -> void:
-	$Control/BorderRect.position = $Control/InnerRect.position - Vector2(border_size,border_size)*4 
-	$Control/BorderRect.size = $Control/InnerRect.size + Vector2(2*border_size, 2*border_size)
-	#print($Control/BorderRect.position, $Control/InnerRect.position)
-	#print($Control/BorderRect.size, $Control/InnerRect.size)
+func _on_timer_timeout() -> void:
+	dialog_ended()
---
+++ scripts/dialog_box.gd
@@ -0,0 +1,28 @@
+extends Control
+
+var border_size : int = 1
+var dialog : String = ""
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	set_border_rect()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func set_dialog(new_dialog : String) -> void:
+	dialog = new_dialog
+	display_dialog()
+
+func display_dialog() -> void:
+	$Dialog.text = dialog
+
+func set_colour(border : Color,inner_rect : Color,text : Color) -> void:
+	pass
+
+func set_border_rect() -> void:
+	$BorderRect.position = $InnerRect.position - Vector2(border_size,border_size)*4 
+	$BorderRect.size = $InnerRect.size + Vector2(2*border_size, 2*border_size)
+	#print($Control/BorderRect.position, $Control/InnerRect.position)
+	#print($Control/BorderRect.size, $Control/InnerRect.size)
