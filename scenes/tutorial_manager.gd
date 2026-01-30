74037f57d258bc6f4755e0546572d5525f9d9922
Quinn Trusler <quinn@trusler.net>
2026-01-30 12:47:36 -0800

    Replace word sunflower with devil in tutorial

--- scenes/tutorial_manager.gd
+++ scenes/tutorial_manager.gd
@@ -7,7 +7,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if Cheats.USE_TUTORIAL:
-		DialogManager.set_dialogs([["Drag the carrot onto the farmland and drop.", -1], ["When the carrots are done growing, click to harvest.", -1], ["Drag a carrot onto the sunflower and drop", -1], ["Round requirments satisfied!",3], ["A new round will start when the timer hits zero.", 7], ["In the meantime stash up on carrots", 7]])
+		DialogManager.set_dialogs([["Drag the carrot onto the farmland and drop.", -1], ["When the carrots are done growing, click to harvest.", -1], ["Drag a carrot onto the devil and drop", -1], ["Round requirments satisfied!",3], ["A new round will start when the timer hits zero.", 7], ["In the meantime stash up on carrots", 7]])
 		next(false, true, false)
 #detect when certain things are done so that the game timer can be stoped/started and so dialog manager can be stopped and started
 
